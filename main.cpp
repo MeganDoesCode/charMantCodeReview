584ea43877ed03ddcf8f8355957b8dbee09c4ffc
matty-michel <mmichel@carthage.edu>
2025-03-22 13:49:44 -0500

    finished multiply function & functions can now handle whole answers
    Added an if statement to check if the answer is a whole number -- if it is, no decimal point/mantissa is added.

--- main.cpp
+++ main.cpp
@@ -41,14 +41,14 @@ int main()
     //initialize the values
     c1 = 1;
     n1 = 1;
-    d1 = 2;
+    d1 = 3;
 
     c2 = 2;
     n2 = 2;
     d2 = 3; 
 
     //if the c-string can hold at least the characteristic
-    if(subtract(c1, n1, d1, c2, n2, d2, answer, 10))
+    if(multiply(c1, n1, d1, c2, n2, d2, answer, 10))
     {
         //display string with answer 4.1666666 (cout stops printing at the null terminating character)
         cout<<"Answer: "<<answer<<endl;
@@ -171,25 +171,29 @@ bool add(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
             }
         }
         
-        //filling result w/ mantissa digits starting from char_length
-        int remain = (answer_num % answer_denom);
-        int dividend = (remain * 10);
-        int digit = (dividend / answer_denom);
-        for(int i = char_length; i < (len - 1); i++)
+        //only add a decimal & mantissa if the answer is not a whole number
+        if(answer_num % answer_denom != 0)
         {
-            //placing . between characteristic & mantissa
-            if(i == char_length)
+            //filling result w/ mantissa digits starting from char_length
+            int remain = (answer_num % answer_denom);
+            int dividend = (remain * 10);
+            int digit = (dividend / answer_denom);
+            for(int i = char_length; i < (len - 1); i++)
             {
-                result[i] = '.';
-            }
-            //setting result at pos i to each digit
-            else
-            {
-                result[i] = '0' + digit;
-                
-                remain = (dividend % answer_denom);
-                dividend = (remain * 10);
-                digit = (dividend / answer_denom);
+                //placing . between characteristic & mantissa
+                if(i == char_length)
+                {
+                    result[i] = '.';
+                }
+                //setting result at pos i to each digit
+                else
+                {
+                    result[i] = '0' + digit;
+                    
+                    remain = (dividend % answer_denom);
+                    dividend = (remain * 10);
+                    digit = (dividend / answer_denom);
+                }
             }
         }
         
@@ -273,25 +277,29 @@ bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
             }
         }
         
-        //filling result w/ mantissa digits starting from char_length
-        int remain = (answer_num % answer_denom);
-        int dividend = (remain * 10);
-        int digit = (dividend / answer_denom);
-        for(int i = char_length; i < (len - 1); i++)
+        //only add a decimal & mantissa if the answer is not a whole number
+        if(answer_num % answer_denom != 0)
         {
-            //placing . between characteristic & mantissa
-            if(i == char_length)
+            //filling result w/ mantissa digits starting from char_length
+            int remain = (answer_num % answer_denom);
+            int dividend = (remain * 10);
+            int digit = (dividend / answer_denom);
+            for(int i = char_length; i < (len - 1); i++)
             {
-                result[i] = '.';
-            }
-            //setting result at pos i to each digit
-            else
-            {
-                result[i] = '0' + digit;
-                
-                remain = (dividend % answer_denom);
-                dividend = (remain * 10);
-                digit = (dividend / answer_denom);
+                //placing . between characteristic & mantissa
+                if(i == char_length)
+                {
+                    result[i] = '.';
+                }
+                //setting result at pos i to each digit
+                else
+                {
+                    result[i] = '0' + digit;
+                    
+                    remain = (dividend % answer_denom);
+                    dividend = (remain * 10);
+                    digit = (dividend / answer_denom);
+                }
             }
         }
         
@@ -301,9 +309,75 @@ bool subtract(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int
 //--
 bool multiply(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
 {
-    //hard coded return value to make the code compile
-    //you will have to come up with an algorithm to multiply the two numbers
-    return true;
+    //checking for 0 division
+    if(d1 == 0 || d2 == 0) {
+        return false;
+    }
+    else
+    {
+        //converting into two fractions
+        int new_num1 = find_numerator(c1, n1, d1);
+        int new_num2 = find_numerator(c2, n2, d2);
+        
+        //multiplying the fractions together
+        int answer_num = new_num1 * new_num2;
+        int answer_denom = d1 * d2;
+        
+        //get the characteristic
+        int char_num = (answer_num / answer_denom);
+        
+        //finding number of digits in characteristic
+        int char_length = find_chara_length(char_num);
+        //returning false if the characteristic exceeds result size
+        if(char_length > (len - 1))
+        {
+            return false;
+        }
+        
+        //filling result w/ characteristic digits
+        for(int i = char_length; i >= 0; i--)
+        {
+            //placing \0 at end of characteristic
+            if(i == char_length)
+            {
+                result[i] = '\0';
+            }
+            //setting result at pos i to each digit
+            else
+            {
+                result[i] = (char_num % 10) + '0';
+                char_num = (char_num / 10);
+            }
+        }
+        
+        //only add a decimal & mantissa if the answer is not a whole number
+        if(answer_num % answer_denom != 0)
+        {
+            //filling result w/ mantissa digits starting from char_length
+            int remain = (answer_num % answer_denom);
+            int dividend = (remain * 10);
+            int digit = (dividend / answer_denom);
+            for(int i = char_length; i < (len - 1); i++)
+            {
+                //placing . between characteristic & mantissa
+                if(i == char_length)
+                {
+                    result[i] = '.';
+                }
+                //setting result at pos i to each digit
+                else
+                {
+                    result[i] = '0' + digit;
+                    
+                    remain = (dividend % answer_denom);
+                    dividend = (remain * 10);
+                    digit = (dividend / answer_denom);
+                }
+            }
+        }
+        
+        return true;
+    }
 }
 //--
 bool divide(int c1, int n1, int d1, int c2, int n2, int d2, char result[], int len)
